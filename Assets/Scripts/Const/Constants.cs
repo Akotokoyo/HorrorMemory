17ad9ec961bb2f934f72c24019077f55126b9d54
akotokoyo <akotokoyo@gmail.com>
2026-08-20 11:19:37 +0200

    [Design] Changed Design parameters

--- Assets/Scripts/Const/Constants.cs
+++ Assets/Scripts/Const/Constants.cs
@@ -1,7 +1,7 @@
 public static class Constants
 {
-    public static float FIRST_STAR_RANGE_PERCENTAGE = 0.50f;
-    public static float SECOND_STAR_RANGE_PERCENTAGE = 0.25f;
+    public static float FIRST_STAR_RANGE_PERCENTAGE = 0.66f;
+    public static float SECOND_STAR_RANGE_PERCENTAGE = 0.33f;
 
     public static int MAX_DIFFERENCES = 15;
     public static int MAX_LANGUAGES = 5;
